875fcd49cb908de8a3384d6f97e40f73dba16b7c
JCTGY <fan3510@gmail.com>
2020-01-03 12:28:46 -0800

    add props

--- component/src/index.js
+++ component/src/index.js
@@ -1,9 +1,32 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
 
 const App = () => {
-	return <div> Hi! there </div>
+	return (
+	<div className="ui container comments">
+	 <CommentDetail 
+	   author="Sam"  
+	   data="8:13AM" 
+	   text="Lota astrat"
+	   avatar={faker.image.avatar()}
+	 />
+	 <CommentDetail 
+	   author="Alex" 
+	   data="11:25AM" 
+	   text="What is that?"
+	   avatar={faker.image.avatar()}
+	 />
+	 <CommentDetail 
+	   author="Reta" 
+	   data="1:13PM" 
+	   text="I have no idea" 
+	   avatar={faker.image.avatar()}
+	 />
+	</div>
+	);
 };
 
 ReactDOM.render(<App/>, document.querySelector('#root'));
