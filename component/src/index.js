d24f7f6d0598155aad7b7467b1459250b8b434cd
JCTGY <fan3510@gmail.com>
2020-01-03 16:19:45 -0800

    add js

--- component/src/index.js
+++ component/src/index.js
@@ -3,28 +3,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
 	return (
 	<div className="ui container comments">
-	 <CommentDetail 
-	   author="Sam"  
-	   data="8:13AM" 
-	   text="Lota astrat"
-	   avatar={faker.image.avatar()}
-	 />
-	 <CommentDetail 
-	   author="Alex" 
-	   data="11:25AM" 
-	   text="What is that?"
-	   avatar={faker.image.avatar()}
-	 />
-	 <CommentDetail 
-	   author="Reta" 
-	   data="1:13PM" 
-	   text="I have no idea" 
-	   avatar={faker.image.avatar()}
-	 />
+	 <ApprovalCard>
+	  <div>
+	   <h4>Warning</h4>
+	   Are you sure you want to do this?
+	  </div>
+	 </ApprovalCard>
+	 <ApprovalCard>
+	  <CommentDetail 
+	    author="Sam"  
+	    data="8:13AM" 
+	    text="Lota astrat"
+	    avatar={faker.image.avatar()}
+	  />
+	 </ApprovalCard>
+	 <ApprovalCard>
+	  <CommentDetail 
+	    author="Alex" 
+	    data="11:25AM" 
+	    text="What is that?"
+	    avatar={faker.image.avatar()}
+	  />
+	 </ApprovalCard>
+	 <ApprovalCard>
+	  <CommentDetail 
+	    author="Reta" 
+	    data="1:13PM" 
+	    text="I have no idea" 
+	    avatar={faker.image.avatar()}
+	  />
+	 </ApprovalCard>
 	</div>
 	);
 };
